c8b1240a5d1183502a78bb16a87de104f36e5eac
Utla, Venkatasubbaiah (ES) <Venkatasubbaiah.Utla@ADP.com>
2019-02-06 16:03:09 +0530

    Setting up maven java project

---
+++ src/main/java/org/venkat/kafka/KafkaStarterApplication.java
@@ -0,0 +1,8 @@
+package org.venkat.kafka;
+
+public class KafkaStarterApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Kafka Starter application is booted!!!");
+    }
+}
